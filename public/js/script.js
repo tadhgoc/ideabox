167ee296dc322c345ad81dbac92b65b8abc89b8c
Sarah Lawrence <s.j.lawrence8@gmail.com>
2017-03-09 15:54:22 +1100

    read json and choose random

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,35 @@
+$(function() {
+  getLanguages('/api/languages');
+});
+
 function tog() {
   $('.idea-modal').addClass("show-me");
+}
+
+
+function getLanguages (url) {
+    fetch(url, {
+        method: 'GET'
+    })
+    .then(function (res) {
+        res.json()
+        .then(function (json) {
+            console.log(json);
+            randomise(json);
+        });
+    })
+    .catch(function (err) {
+        console.error(err)
+    });
+}
+
+function randomise(json) {
+
+  // make a random number
+  var random = (Math.random() * 1000);
+
+  // pick index
+  var pick =  parseInt(random) % json.length;
+
+  return json[pick];
 }
