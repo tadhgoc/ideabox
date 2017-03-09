51c759c1e76af2b9524758d99d224d653e3ee6d7
Tadhg O'Callaghan <tadhg.ocallaghan@mi9..com.au>
2017-03-10 10:49:09 +1100

    resources endpoint

--- api/resources.js
+++ api/resources.js
@@ -2,7 +2,13 @@
 const resources = require("../utils/resources");
 
 module.exports = (req, res, next) => {
-  const language = req.query.lang;
+  const language = req.query.lang.toLowerCase();
+  console.log('language', `>>${encodeURIComponent(language)}<<`);
 
-  res.json(resources[language]);
+  let resource = resources[language];
+  if(resource === undefined) {
+    resource = `https://www.google.com.au/search?q=${encodeURIComponent(language)}`;
+  }
+
+  res.json(resource);
 }
--- utils/resources.js
+++ utils/resources.js
@@ -1,11 +1,11 @@
 module.exports = {
-  "Python": "https://www.python.org/",
-  "JavaScript": "",
-  "Java": "",
-  "Go": "",
-  "C++": "",
-  "TeX": "",
-  "C": "",
-  "Vue": "",
-  "TypeScript": ""
+  "python": "https://www.python.org/about/gettingstarted/",
+  "javascript": "https://www.javascript.com/",
+  "java": "http://www.oracle.com/technetwork/java/index-138747.html",
+  "go": "https://github.com/golang/go",
+  "c++": "http://www.cplusplus.com/doc/tutorial/",
+  "tex": "https://www.tug.org/",
+  "c": "http://www.cprogramming.com/begin.html",
+  "vue": "https://github.com/vuejs/vue",
+  "typescript": "https://github.com/Microsoft/TypeScript"
 };
