077a306eb6f91c51ffa776172d1f5dbcb97b84b5
Tadhg O'Callaghan <tadhg.ocallaghan@mi9..com.au>
2017-03-10 09:22:17 +1100

    twitter backend integration

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 var languages = require('./languages');
 var words = require('./words');
 var randomise = require('./randomise');
+var twitter = require('./twitter');
 var express = require('express');
 
 var router = express.Router();
@@ -8,5 +9,6 @@ var router = express.Router();
 router.get('/languages', languages);
 router.get('/words', words);
 router.get('/randomise', randomise);
+router.get('/twitter', twitter);
 
 module.exports = router;
---
+++ api/twitter.js
@@ -0,0 +1,19 @@
+"use strict"
+const Twitter = require('twitter');
+
+const client = new Twitter({
+  consumer_key: 'VzuHqyyEKNaXcuiyP1D1HOd4V',
+  consumer_secret: 'dCo8ENNnX4D9wLMGo1n4VfWdbC4MQ1sn3z4g0OrYZ1zL8lMsNq',
+  access_token_key: '839955319635628033-9LD9YSKjLp0Jo3PwL1hSTI0d3Nk6x5N',
+  access_token_secret: 'kZ9mwaPRDEDdngMKOGKV6FMqoZuODurmYYFeP52NbUt64'
+});
+
+
+module.exports = (req, res, next) => {
+  const params = req.query;
+  client.get('search/tweets', params, function(error, tweets, response) {
+    if(error) return;
+
+    res.json(tweets);
+  });
+}
