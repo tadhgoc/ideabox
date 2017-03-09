7da919b9852889d0058d299665ddd5d03aaafe9e
Sarah Lawrence <s.j.lawrence8@gmail.com>
2017-03-09 20:23:55 +1100

    added prelim styling for idea text

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,6 @@
 $(function() {
   getData('/api/randomise');
+  $('.idea-text').hide();
 });
 
 function getData (api) {
@@ -17,17 +18,19 @@ function getData (api) {
     });
 }
 
-function tog() {
-  $('.box').addClass("shake");
-  $('.idea').addClass("fly");
-  // $('.idea-modal').addClass("show-me");
-
-
-}
-
 function buildString(data) {
   result = "Use " + data.language + " to make " + data.word;
   console.log(result);
 
   $(".idea-text").text(result);
+}
+
+function tog() {
+  $('.box').addClass("shake");
+  $('.idea').addClass("fly");
+}
+
+function showIdea() {
+  console.log("called show idea");
+  $('.idea-text').show();
 }
